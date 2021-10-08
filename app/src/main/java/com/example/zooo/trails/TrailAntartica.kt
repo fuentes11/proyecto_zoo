0a44dd5bc965a061b7c3269a8cddd3b5f3f33493
Diego Sandoval <sandovaldiego21@gmail.com>
2021-10-07 21:08:14 -0600

    siu

---
+++ app/src/main/java/com/example/zooo/trails/TrailAntartica.kt
@@ -0,0 +1,12 @@
+package com.example.zooo.trails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zooo.R
+
+class TrailAntartica : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trail_antartica)
+    }
+}
---
+++ app/src/main/java/com/example/zooo/trails/TrailAsia.kt
@@ -0,0 +1,12 @@
+package com.example.zooo.trails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zooo.R
+
+class TrailAsia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trail_asia)
+    }
+}
