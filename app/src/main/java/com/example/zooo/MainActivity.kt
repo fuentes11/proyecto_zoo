920b141b52e59c64a2fc4d205762f3b4a0e4f47f
Diego Sandoval <sandovaldiego21@gmail.com>
2021-10-07 18:50:49 -0600

    si

--- app/src/main/java/com/example/zooo/MainActivity.kt
+++ app/src/main/java/com/example/zooo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.zooo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zooo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.btn2.setOnClickListener {
+            startActivity(Intent(this,CollectionMain::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/zooo/TrailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zooo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trails)
+    }
+}
