f9d387ed6635d308c4a66cb9eb315425bb9528c3
Diego Sandoval <sandovaldiego21@gmail.com>
2021-10-09 17:17:14 -0600

    siu

--- app/src/main/java/com/example/zooo/TrailsActivity.kt
+++ app/src/main/java/com/example/zooo/TrailsActivity.kt
@@ -1,11 +1,25 @@
 package com.example.zooo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zooo.databinding.ActivityMainBinding
+import com.example.zooo.databinding.ActivityTrailsBinding
 
-class TrailsActivity : AppCompatActivity() {
+class TrailsActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityTrailsBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_trails)
+        binding = ActivityTrailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        addListeners()
     }
+
+    private fun addListeners() {
+       binding.fbHome.setOnClickListener{
+           startActivity(Intent(this,MainActivity::class.java))
+       }
+    }
+
+
 }
