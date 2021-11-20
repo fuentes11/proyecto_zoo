cbb49fe82fe1fe22ef7bd6438c32620cc388fe50
Diego Sandoval <sandovaldiego21@gmail.com>
2021-11-19 22:59:01 -0600

    siu

---
+++ app/src/main/java/com/example/zooo/RulesActivity.kt
@@ -0,0 +1,23 @@
+package com.example.zooo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zooo.databinding.ActivityPricesBinding
+import com.example.zooo.databinding.ActivityRulesBinding
+
+class RulesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRulesBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRulesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.fbHome.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
